d3868c6a082ec9346918efc71e6fe4da2cca662e
elleon003 <ngrandison@ymail.com>
2020-05-03 21:25:59 -0400

    Add Top Row Homepage Admin

--- wagtail/home/models.py
+++ wagtail/home/models.py
@@ -1,7 +1,69 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
-    pass
+    # First Row content
+    top_image = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=False,
+        null=True,
+        related_name='+',
+        on_delete=models.SET_NULL,
+        help_text='The top, primary image'
+    )
+    about_header = models.CharField(
+        blank=False,
+        null=True,
+        help_text='Header for the about page teaser',
+        max_length=140,
+    )
+    lead_text = models.CharField(
+        blank=False,
+        null=True,
+        max_length=500, 
+        help_text='Intro to about section text',
+    )
+    linked_page = models.ForeignKey(
+        'wagtailcore.Page',
+        blank=True,
+        null=True,
+        related_name="+",
+        help_text='Select a page to link to',
+        on_delete=models.SET_NULL,
+    )
+    linked_page_text = models.CharField(
+        max_length=50,
+        default='Read More',
+        blank=False,
+        help_text='Link text',
+    )
+
+    # TODO - second row content
+        # Img Uploader
+        # Most Recent Post Heading
+        # Most Recent Post Blurb
+        # Link to Post
+
+    # TODO Third Row - Contact info
+
+
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                ImageChooserPanel("top_image"),
+                FieldPanel("about_header"),
+                FieldPanel("lead_text"),
+                PageChooserPanel("linked_page"),
+                FieldPanel("linked_page_text"),
+            ],
+            heading="First Row",
+            classname="collapsible",
+        )
+    ]
+
+    parent_page_types = []
